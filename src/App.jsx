b562bd80a8ac0b1056dfd489988b5dc256fc94b0
Atharv Bapat <atharvabapat3@gmail.com>
2025-12-17 09:28:04 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import ServiceDetails from "./pages/ServiceDetails";
 import "./styles.css";
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Hero from "../components/Hero";
-import Services from "../components/Services";
-import HowItWorks from "../components/HowItWorks";
+import Hero from "../components/Hero.jsx";
+import Services from "../components/Services.jsx";
+import HowItWorks from "../components/HowItWorks.jsx";
 
 import Contact from "./Contact";
 
