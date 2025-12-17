d91bf8e2cbf513aee48d6b81b6a3e419bd334492
Atharv Bapat <atharvabapat3@gmail.com>
2025-12-17 10:06:25 +0530

    fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import ServiceDetails from "./pages/ServiceDetails";
+import Home from "./Pages/Home.jsx";
+import ServiceDetails from "./Pages/ServiceDetails";
 import "./styles.css";
 
 
